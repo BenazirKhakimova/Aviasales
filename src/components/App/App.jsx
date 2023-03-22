dbf91061445c3409a97c18aabee4b76460a5541e
Benazir Khakimova <benazirkhakimova91@gmail.com>
2023-03-22 11:39:11 +0600

    init

---
+++ src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styles from './App.module';
+
+const App = (props) => (
+  <div className="AppWrapper">
+    Test content
+  </div>
+);
+
+App.propTypes = {
+  // bla: PropTypes.string,
+};
+
+App.defaultProps = {
+  // bla: 'test',
+};
+
+export default App;
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+//import styles from './Logo.module.scss';
+
+const Logo = (props) => (
+  <div className="LogoWrapper">
+    Test content
+  </div>
+);
+
+Logo.propTypes = {
+  // bla: PropTypes.string,
+};
+
+Logo.defaultProps = {
+  // bla: 'test',
+};
+
+export default Logo;
